75f9b1486958405430ef0b0d912d70f964f8ac1c
Mir Ali <bachchu333@gmail.com>
2024-06-26 11:50:52 +0530

    java spring app added

---
+++ programming-basics-learn/java-spring-boot/javaSpringProj/src/main/java/com/mir_owahed/javaSpringProj/JavaSpringProjApplication.java
@@ -0,0 +1,13 @@
+package com.mir_owahed.javaSpringProj;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaSpringProjApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaSpringProjApplication.class, args);
+	}
+
+}
---
+++ programming-basics-learn/java-spring-boot/javaSpringProj/src/test/java/com/mir_owahed/javaSpringProj/JavaSpringProjApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mir_owahed.javaSpringProj;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaSpringProjApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
